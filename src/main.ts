af11215c99e93e51f6cbdb6657c951c9676b6b08
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2026-01-10 22:05:23 -0500

    changes to make push work

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
-// Import phila-ui-core CSS for design system variables and button styles
-// import '@phila/phila-ui-core/dist/styles/variables.css'
-// import '@phila/phila-ui-core/dist/styles/light-mode.css'
-// import '@phila/phila-ui-core/dist/styles/elements/buttons.css'
+// import "@phila/phila-ui-core/styles/variables.css";
+// import "@phila/phila-ui-core/styles/light-mode.css";
 
 // FontAwesome setup
 import { library } from '@fortawesome/fontawesome-svg-core'
